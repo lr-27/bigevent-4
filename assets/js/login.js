5275b82c54aac24f030245e6697cfa3b1d229367
wenbo <1111111@qq.com>
2020-07-31 23:05:40 +0800

    login页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,65 +1,57 @@
 $(function () {
-    // 切换登录和注册页面
-    $('.link_reg').on('click', function () {
-        // console.log(123);
+    $('#reg').on('click', function () {
         $('#login_form').hide()
         $('#reg_form').show()
-
     })
-    $('.link_login').on('click', function () {
+    $('#login').on('click', function () {
         $('#login_form').show()
         $('#reg_form').hide()
     })
-    // console.log($('#repwd').val())
-    // 设置密码验证规则
+    // 定义校验规则
     var form = layui.form;
     var layer = layui.layer;
     form.verify({
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
         repwd: function (value) {
-            var pwd = $('#repwd').val();
-            if (pwd !== value) {
-                return '两次输入不一致'
-            }
+            var pwd = $('#jypwd').val()
+            if (pwd !== value) return '两次输入密码不一致';
         }
     })
+})
 
-    // ajax发送请求注册成功后将信息提交到后台并且跳转到登录页面
-    $('#reg_form').on('submit', function (e) {
-        e.preventDefault();
-        console.log($(this).serialize());
-        $.ajax({
-            type: 'post',
-            url: '/api/reguser',
-            data: $(this).serialize(),
-            success: function (res) {
-                // console.log(res);
-                if (res.status !== 0) return layer.msg(res.message)
-                layer.msg(res.message)
-                $('#reg_form')[0].reset()
-                $('.link_login').click()
-                // location.href = '/index.html'
-            }
-        })
+// 注册提交的时候发送ajax请求提交表单
+$('#reg_form').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+        type: 'post',
+        url: '/api/reguser',
+        data: $(this).serialize(),
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) return layer.msg(res.message)
+            layer.msg('注册成功')
+            $('#reg_form')[0].reset()
+            $('#login').click()
 
+        }
     })
-
-    // 发送ajax请求请求成功后跳转页面
-
-    $('#login_form').on('submit', function (e) {
-        e.preventDefault();
-        console.log($(this).serialize());
-        $.ajax({
-            type: 'post',
-            url: '/api/login',
-            data: $(this).serialize(),
-            success: function (res) {
-                if (res.status !== 0) return layer.msg(res.message)
-                layer.msg(res.message)
-                localStorage.setItem('token', res.token)
-                location.href = '/index.html'
-            }
-        })
-
+})
+//  用户登录的时候发送请求到index页面
+$('#login_form').on('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+        type: 'post',
+        url: '/api/login',
+        data: $(this).serialize(),
+        success: function (res) {
+            // console.log(res);
+            if (res.status !== 0) return layer.msg(res.message)
+            layer.msg('登录成功')
+            localStorage.setItem('token', res.token);
+            location.href = '/index.html'
+        }
     })
 })
